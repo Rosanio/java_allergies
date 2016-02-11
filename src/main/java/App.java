6166e214ae2c7fad56159d572f4143ac93aba3a0
Daren Schaad and Matt Rosanio <darenschaad@gmail.com>
2016-02-11 11:40:03 -0800

    add and pass integration test to see if webpage is loading

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,14 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/allergy-score.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 
   public static ArrayList checkAllergies(Integer allergyScore) {
     HashMap<Integer, String> allergyIndex = new HashMap<Integer, String>();
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -20,11 +20,11 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Test
-  // public void rootTest() {
-  //     goTo("http://localhost:4567/");
-  //     assertThat(pageSource()).contains("");
-  // }
+  @Test
+  public void rootTest() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("Enter your allergy score");
+  }
 
   @Test
   public void checkAllergies_catAllergies_returnCatAllergies() {
